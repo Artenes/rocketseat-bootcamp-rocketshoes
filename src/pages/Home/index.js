091d9d40b526a5d6dfebe4874148f791e4e1616a
Artenes Nogueira <artenesama@gmail.com>
2019-09-15 10:26:25 -0400

    chore: Creates structure and style for list
    yarn add polished
    
    Guess what? We used a whole library just to darken a color in css.
    
    &:hover {
            background: ${darken(0.03, '#7159c1')};
          }

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-fly-by-low-ii-masculino/99/HZM-0832-999/HZM-0832-999_detalhe2.jpg?ims=326x"
+          alt="Tennis"
+        />
+        <strong>Cools tennis for walks</strong>
+        <span>$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-fly-by-low-ii-masculino/99/HZM-0832-999/HZM-0832-999_detalhe2.jpg?ims=326x"
+          alt="Tennis"
+        />
+        <strong>Cools tennis for walks</strong>
+        <span>$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-fly-by-low-ii-masculino/99/HZM-0832-999/HZM-0832-999_detalhe2.jpg?ims=326x"
+          alt="Tennis"
+        />
+        <strong>Cools tennis for walks</strong>
+        <span>$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-fly-by-low-ii-masculino/99/HZM-0832-999/HZM-0832-999_detalhe2.jpg?ims=326x"
+          alt="Tennis"
+        />
+        <strong>Cools tennis for walks</strong>
+        <span>$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-fly-by-low-ii-masculino/99/HZM-0832-999/HZM-0832-999_detalhe2.jpg?ims=326x"
+          alt="Tennis"
+        />
+        <strong>Cools tennis for walks</strong>
+        <span>$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-fly-by-low-ii-masculino/99/HZM-0832-999/HZM-0832-999_detalhe2.jpg?ims=326x"
+          alt="Tennis"
+        />
+        <strong>Cools tennis for walks</strong>
+        <span>$129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
