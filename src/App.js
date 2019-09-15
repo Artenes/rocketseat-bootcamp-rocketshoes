32f38f9927c1c99d01c682c3c876a94d368b3c4d
Artenes Nogueira <artenesama@gmail.com>
2019-09-15 09:11:10 -0400

    chore: Creates global style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <GlobalStyle />
       {/** Header */}
       <Routes />
     </BrowserRouter>
