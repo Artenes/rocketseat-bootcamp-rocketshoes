8604ec3f42126ed794f87ec71e1b604dd190d8d0
Artenes Nogueira <artenesama@gmail.com>
2019-09-15 21:12:48 -0400

    Updates

---
+++ precache-manifest.bacf57c790b883b9320456075c4da44c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c01deefb2e9a49ddcc40607f9a5475",
+    "url": "/rocketseat-bootcamp-rocketshoes/index.html"
+  },
+  {
+    "revision": "34e93d0baad26f138351",
+    "url": "/rocketseat-bootcamp-rocketshoes/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "34e93d0baad26f138351",
+    "url": "/rocketseat-bootcamp-rocketshoes/static/js/2.edf7e1d9.chunk.js"
+  },
+  {
+    "revision": "373debc309b74dc3c8f5",
+    "url": "/rocketseat-bootcamp-rocketshoes/static/js/main.df9cc3d5.chunk.js"
+  },
+  {
+    "revision": "60e176c466e509d27bce",
+    "url": "/rocketseat-bootcamp-rocketshoes/static/js/runtime~main.edad4553.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/rocketseat-bootcamp-rocketshoes/static/media/background.3c9a1ce5.svg"
+  },
+  {
+    "revision": "879557c5bdbbd3743a7680c651e31d08",
+    "url": "/rocketseat-bootcamp-rocketshoes/static/media/logo.879557c5.svg"
+  }
+]);
