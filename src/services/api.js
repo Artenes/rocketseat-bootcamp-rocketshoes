df8d60e17961b64e3dcf843e18652b9c5e2a145f
Artenes Nogueira <artenesama@gmail.com>
2019-09-15 20:58:39 -0400

    chore: Create fakeApi for production deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,35 @@
 import axios from 'axios';
+import * as fakeData from '../server.json';
+
+const fakeApi = {
+  get(url) {
+    const [path, param] = url.split('/');
+
+    if (url === 'products') {
+      return { data: fakeData.products };
+    }
+
+    if (path === 'products' && param) {
+      const data = fakeData.products.find(p => p.id === Number(param));
+      return { data };
+    }
+
+    if (path === 'stock' && param) {
+      const data = fakeData.stock.find(s => s.id === Number(param));
+      return { data };
+    }
+
+    return { data: null, status: 404 };
+  },
+};
 
 const api = axios.create({
   baseURL: 'http://localhost:3333/',
 });
 
-export default api;
+/**
+ * The production build of this app will be deployed
+ * without a back-end, that's why we use the fake api
+ * outside the development environment.
+ */
+export default process.env.NODE_ENV === 'development' ? api : fakeApi;
